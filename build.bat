00db4b7410120a7726bb668ac23f5768afe56315
Filter <not.so.filter@hotmail.com>
2025-12-03 15:46:20 -0500

    save space

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-if exist flicky.bin move /Y flicky.bin flicky.prev.bin >NUL
+if exist flicky.bin move /y flicky.bin flicky.prev.bin >NUL
 tools\asw -xx -q -A -L -E -i . flicky.asm
 tools\p2bin -p=FF flicky.p flicky.bin
 del flicky.p
